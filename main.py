630f659e0dee0e56d9cb1a6288f2568c5d2a8487
A2P1 <pabloazpitartegutierrez@gmail.com>
2024-12-04 17:05:27 +0100

    Inversión inicial hecha

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 
 
+def main():
+    menu()
+
+def menu():
+    print("Bienvenido a la simulacion de inversion")
+    monto = int(input("Primero ingrese el monto inicial para la inversión"))
+    interes = float(input("A continuación, introduzca el porcentaje en decimal de la tasa de interés anual"))
+    tiempo = int(input("Por último, ingrese el tiempo en años de la inversión"))
+
+    CantidadFinal = inversion(monto, interes, tiempo)
+    print("La Cantidad final es: ", CantidadFinal)
+def inversion(monto, interes, tiempo):
+    CantidadFinal = monto * (1 + interes) ** tiempo #Formula de la inversión
+    return CantidadFinal
+
+main()
